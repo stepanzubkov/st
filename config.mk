0078cb973345fcc690466d0617ee150289a00927
Stepan Zubkov <zubkovbackend@gmail.com>
2024-04-13 11:25:48 +0300

    feat: edit config.mk.

--- config.mk
+++ config.mk
@@ -4,11 +4,11 @@ VERSION = 0.8.5
 # Customize below to fit your system
 
 # paths
-PREFIX = /usr/local
+PREFIX = /usr
 MANPREFIX = $(PREFIX)/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC = /usr/include/X11
+X11LIB = /usr/lib/X11
 
 PKG_CONFIG = pkg-config
 
@@ -34,4 +34,4 @@ STLDFLAGS = $(LIBS) $(LDFLAGS)
 #       `$(PKG_CONFIG) --libs freetype2`
 
 # compiler and linker
-# CC = c99
+CC = gcc
